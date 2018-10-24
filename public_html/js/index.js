1f613aa23cbcd0220ec8955ea2d8dfeb9ce9aba9
Roman <romanm@paulbunyan.net>
2018-10-24 09:23:51 -0500

    added webpack to compile js and sass

---
+++ public_html/js/index.js
@@ -0,0 +1,2 @@
+require('/Users/romanmartushev/interactive-story/node_modules/laravel-mix/src/index');
+Config.publicPath = 'public_html';
---
+++ resources/js/main.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+new Vue({
+   el: '#root',
+   data: {
+       message: 'Hello world'
+   }
+});
