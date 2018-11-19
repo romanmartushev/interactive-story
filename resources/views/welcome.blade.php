8e27c13620797b8bda34ea139ff7891b0a3a8089
Roman <romanm@paulbunyan.net>
2018-11-19 13:18:48 -0600

    I have "finished" this project for now. This will be the final copy turned in as an assignment

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -43,9 +43,6 @@
                 @include('charlie.charlie8')
             </transition>
             <transition name="fade" v-cloak>
-                @include('charlie.charlieImages')
-            </transition>
-            <transition name="fade" v-cloak>
                 @include('partials.final')
             </transition>
         </div>
